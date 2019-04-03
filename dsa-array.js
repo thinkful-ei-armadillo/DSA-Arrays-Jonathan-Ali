33c44eb08929329950c97fc141c4d417633d070d
Ali Lahrime <alahrime@email.wm.edu>
2019-04-03 11:42:44 -0400

    started array module

---
+++ dsa-array.js
@@ -0,0 +1,71 @@
+const memory = require("./memory.js");
+
+let Memory = new memory();
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+
+    this.ptr = Memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    Memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = Memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error("Out of Memory");
+    }
+    Memory.copy(this.ptr, oldPtr, this.length);
+    Memory.free(oldPtr);
+    this._capacity = size;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index error");
+    }
+    return Memory.get(this.ptr + index);
+  }
+  pop() {
+    if (this.length == 0) {
+      throw new Error("Index error");
+    }
+    const value = Memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index error");
+    }
+    Memory.copy(
+      this.ptr + index,
+      this.ptr + index + 1,
+      this.length - index - 1
+    );
+    this.length--;
+  }
+}
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+
+  console.log(arr);
+}
+
+main();
